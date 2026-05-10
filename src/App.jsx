f57af404d231941a965bd744cecf44a770eb16c6
muzammilhusainid <muzammilhusainid@gmail.com>
2026-05-10 23:49:51 +0530

    Update alignment

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import { Form } from 'react-router-dom'
 
 const App = () => {
   return (
-    <div>
+    <div className="flex justify-center">
       <BrowserRouter>
       <ScrollToTop/>
         <Routes>
